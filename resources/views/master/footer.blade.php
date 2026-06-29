d10d8301d4b07098b6d62392be2c4e6c6ccc44f5
haidarharfi552 <bat.siatab@gmail.com>
2026-06-29 11:09:45 +0700

    merubah link footer 29 Juni 2026

--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -15,10 +15,10 @@
                 </p> -->
                 <div class="footer-social-links" aria-label="Media sosial Balai Air Tanah">
                     <ul>
-                        <li><a href="https://x.com/pupr_sda" target="_blank" rel="noopener noreferrer" aria-label="X"><i class="fa-brands fa-x-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="https://x.com/pu_sda_bat" target="_blank" rel="noopener noreferrer" aria-label="X"><i class="fa-brands fa-x-twitter" aria-hidden="true"></i></a></li>
                         <li><a href="https://www.instagram.com/pu_sda_balaiairtanah/" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a></li>
-                        <li><a href="https://www.facebook.com/p/balaiairtanah-100063971832730/" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><i class="fa-brands fa-facebook-f" aria-hidden="true"></i></a></li>
-                        <li><a href="https://www.youtube.com/@pu_sda" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><i class="fa-brands fa-youtube" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.facebook.com/pupr.sda.balaiairtanah" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><i class="fa-brands fa-facebook-f" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.youtube.com/@pu_sda_balaiairtanah" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><i class="fa-brands fa-youtube" aria-hidden="true"></i></a></li>
                     </ul>
                 </div>
             </div>
